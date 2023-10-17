220fdd32747dac4611e731f156d4508d6cc60367
Alexander Moshnitsky <pitonthesadboy@gmail.com>
2023-10-17 14:21:22 +0400

    several changes

--- src/Components/TotalCostDisplay.jsx
+++ src/Components/TotalCostDisplay.jsx
@@ -9,9 +9,9 @@ const TotalCostDisplay = () => {
 
   return (
     <div>
-      <h3>Total Energy Cost: {totalEnergyCost.toFixed(2)} GEL</h3>
+      <h3>Total Energy Cost: GEL {totalEnergyCost.toFixed(2)}</h3>
       <h3>Total Fertilizer Cost: GEL {totalFertilizerCost.toFixed(2)}</h3>
-      <h2>Total cost: GEL {totalCost.toFixed(2)} </h2>
+      <h2>Total Grow Expenses: GEL {totalCost.toFixed(2)} </h2>
     </div>
   );
 };
